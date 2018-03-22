302420f7d056ac7ad73b171604f364328ba53b79
Ronaldo_Martins <ronaldomartins06@gmail.com>
2018-03-22 15:57:40 +0000

    LM

--- dataMiningSales.R
+++ dataMiningSales.R
@@ -2,3 +2,12 @@ file <- 'C:/Users/Ronaldo Martins/Documents/University/Data_Web_Mining/CA_2_Data
 DataFrame <- read.csv(file, sep=",",header = T)
 head(DataFrame)
 ##############
+
+cor(SalesPred$date_block_num, SalesPred$item_cnt_day)
+
+Model <- lm(date_block_num ~ item_cnt_day, data=SalesPred)
+Model
+
+shapiro.test(SalesPred$item_cnt_day)
+
+boxplot(SalesPred$item_cnt_day)

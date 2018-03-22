3c6f31f8dc2ab354fd70fe678f7356f5659678d1
Ronaldo_Martins <ronaldomartins06@gmail.com>
2018-03-22 15:55:47 +0000

    This code is just to read the file sales.

---
+++ dataMiningSales.R
@@ -0,0 +1,4 @@
+file <- 'C:/Users/Ronaldo Martins/Documents/University/Data_Web_Mining/CA_2_Data/sales_train_v2.csv'
+DataFrame <- read.csv(file, sep=",",header = T)
+head(DataFrame)
+##############
